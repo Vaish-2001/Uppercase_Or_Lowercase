6f5fdb86d862d83e9ad5ba00d586dfaa265645a1
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-02-23 12:08:30 +0530

    Add files via upload

---
+++ Uppercase.java
@@ -0,0 +1,9 @@
+class Uppercase
+{ 
+  public static void main(String[]args)
+   {
+    char n='G';
+    String result=(n>=65&&n<=90)?("the given value is uppercase"):("the given value is lowercase");
+    System.out.println(result);
+   }
+}
